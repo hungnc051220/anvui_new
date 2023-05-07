8dd0f01941911acc008ad9993dc37e089057d35c
Hungken <hungnc051220@gmail.com>
2023-05-07 19:56:44 +0700

    fix bugs

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,15 @@
-import createMiddleware from 'next-intl/middleware';
- 
+import createMiddleware from "next-intl/middleware";
+
 export default createMiddleware({
   // A list of all locales that are supported
-  locales: ['vi', 'en'],
- 
+  locales: ["vi", "en"],
+
   // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
-  defaultLocale: 'vi'
+  defaultLocale: "vi",
+  localeDetection: false,
 });
- 
+
 export const config = {
   // Skip all paths that should not be internationalized
-  matcher: ['/((?!api|_next|.*\\..*).*)']
+  matcher: ["/((?!api|_next|.*\\..*).*)"],
 };
